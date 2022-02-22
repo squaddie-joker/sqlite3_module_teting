9bfa6f8016e3e51fffbfe110c331593db70a714b
Squaddie Joker <squaddie-joker@yandex.ru>
2022-02-22 15:59:19 +1000

    Add function "exequte_query"

--- module_sqlite3/cities.py
+++ module_sqlite3/cities.py
@@ -21,21 +21,62 @@ def create_connection(db_path : str) -> sqlite3.Connection:
 
     try:
         connection = sqlite3.connect(db_path)
-        print("Successfully connection!")
+        print("Successful connection!")
     except sqlite3.Error as err:
         print(f"Error! {err}")
 
     return connection 
 
 
+def exequte_query(connection : sqlite3.Connection, query : str) -> bool:
+    result = False
+
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Successful execution!")
+        result = True
+    except sqlite3.Error as err:
+        print(f"Error execution! {err}")
+
+    return result
+
+
+# Get cites list.
+F_CITIES_PATH = r'module_sqlite3\spisok.csv'
+city_set = get_cities_from_file(F_CITIES_PATH)
+
+# Create connection to DB.
 DB_PATH = r'module_sqlite3\my_db.db'
 connection = create_connection(DB_PATH)
 
 
-'''
-F_CITIES_PATH = r'spisok.csv'
-city_set = get_cities_from_file(F_CITIES_PATH)
+create_cities_table = """
+CREATE TABLE IF NOT EXISTS cities (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  city_name   TEXT NOT NULL,
+  region_name TEXT NOT NULL
+);
+"""
+result = exequte_query(connection, create_cities_table)
 
-for item in city_set:
-    print(item)
-'''
+# Adding values to cities table.
+add_values_to_cities_table = """
+INSERT INTO
+  cities (city_name, region_name)
+VALUES
+"""
+for city in city_set:
+    city_name, region_name = city
+    query = add_values_to_cities_table + f"\n('{city_name}', '{region_name}');"
+    result = exequte_query(connection, query)
+
+
+
+
+
+
+
+
+
